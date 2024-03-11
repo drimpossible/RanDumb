feae40c9f7182ad1b7825ed293e99400dbd33436
Ameya Prabhu <drimpossible@mailfence.com>
2024-03-11 08:10:09 +0100

    Update runall.sh

--- scripts/runall.sh
+++ scripts/runall.sh
@@ -1,1 +1,231 @@
+# RanDumb -- All Runs
+python main.py --embed --embed_dim 1000 --model SLDA --dataset mnist
+python main.py --embed --embed_dim 1000 --model SLDA --dataset cifar10
+python main.py --embed --embed_dim 1000 --model SLDA --dataset cifar100
+python main.py --embed --embed_dim 1000 --model SLDA --dataset tinyimagenet_smol
+python main.py --embed --embed_dim 1000 --model SLDA --dataset miniimagenet_smol
+python main.py --embed --embed_dim 1000 --augment --model SLDA --dataset cifar10
+python main.py --embed --embed_dim 1000 --augment --model SLDA --dataset cifar100
+python main.py --embed --embed_dim 1000 --augment --model SLDA --dataset tinyimagenet_smol
+python main.py --embed --embed_dim 1000 --augment --model SLDA --dataset miniimagenet_smol
+python main.py --embed --embed_dim 1000 --model NCM --dataset mnist
+python main.py --embed --embed_dim 1000 --model NCM --dataset cifar10
+python main.py --embed --embed_dim 1000 --model NCM --dataset cifar100
+python main.py --embed --embed_dim 1000 --model NCM --dataset tinyimagenet_smol
+python main.py --embed --embed_dim 1000 --model NCM --dataset miniimagenet_smol
+python main.py --embed --embed_dim 1000 --augment --model NCM --dataset cifar10
+python main.py --embed --embed_dim 1000 --augment --model NCM --dataset cifar100
+python main.py --embed --embed_dim 1000 --augment --model NCM --dataset tinyimagenet_smol
+python main.py --embed --embed_dim 1000 --augment --model NCM --dataset miniimagenet_smol
+python main.py --embed --embed_dim 2000 --model SLDA --dataset mnist
+python main.py --embed --embed_dim 2000 --model SLDA --dataset cifar10
+python main.py --embed --embed_dim 2000 --model SLDA --dataset cifar100
+python main.py --embed --embed_dim 2000 --model SLDA --dataset tinyimagenet_smol
+python main.py --embed --embed_dim 2000 --model SLDA --dataset miniimagenet_smol
+python main.py --embed --embed_dim 2000 --augment --model SLDA --dataset cifar10
+python main.py --embed --embed_dim 2000 --augment --model SLDA --dataset cifar100
+python main.py --embed --embed_dim 2000 --augment --model SLDA --dataset tinyimagenet_smol
+python main.py --embed --embed_dim 2000 --augment --model SLDA --dataset miniimagenet_smol
+python main.py --embed --embed_dim 2000 --model NCM --dataset mnist
+python main.py --embed --embed_dim 2000 --model NCM --dataset cifar10
+python main.py --embed --embed_dim 2000 --model NCM --dataset cifar100
+python main.py --embed --embed_dim 2000 --model NCM --dataset tinyimagenet_smol
+python main.py --embed --embed_dim 2000 --model NCM --dataset miniimagenet_smol
+python main.py --embed --embed_dim 2000 --augment --model NCM --dataset cifar10
+python main.py --embed --embed_dim 2000 --augment --model NCM --dataset cifar100
+python main.py --embed --embed_dim 2000 --augment --model NCM --dataset tinyimagenet_smol
+python main.py --embed --embed_dim 2000 --augment --model NCM --dataset miniimagenet_smol
+python main.py --embed --embed_dim 5000 --model SLDA --dataset mnist
+python main.py --embed --embed_dim 5000 --model SLDA --dataset cifar10
+python main.py --embed --embed_dim 5000 --model SLDA --dataset cifar100
+python main.py --embed --embed_dim 5000 --model SLDA --dataset tinyimagenet_smol
+python main.py --embed --embed_dim 5000 --model SLDA --dataset miniimagenet_smol
+python main.py --embed --embed_dim 5000 --augment --model SLDA --dataset cifar10
+python main.py --embed --embed_dim 5000 --augment --model SLDA --dataset cifar100
+python main.py --embed --embed_dim 5000 --augment --model SLDA --dataset tinyimagenet_smol
+python main.py --embed --embed_dim 5000 --augment --model SLDA --dataset miniimagenet_smol
+python main.py --embed --embed_dim 5000 --model NCM --dataset mnist
+python main.py --embed --embed_dim 5000 --model NCM --dataset cifar10
+python main.py --embed --embed_dim 5000 --model NCM --dataset cifar100
+python main.py --embed --embed_dim 5000 --model NCM --dataset tinyimagenet_smol
+python main.py --embed --embed_dim 5000 --model NCM --dataset miniimagenet_smol
+python main.py --embed --embed_dim 5000 --augment --model NCM --dataset cifar10
+python main.py --embed --embed_dim 5000 --augment --model NCM --dataset cifar100
+python main.py --embed --embed_dim 5000 --augment --model NCM --dataset tinyimagenet_smol
+python main.py --embed --embed_dim 5000 --augment --model NCM --dataset miniimagenet_smol
+python main.py --embed --embed_dim 10000 --model SLDA --dataset mnist
+python main.py --embed --embed_dim 10000 --model SLDA --dataset cifar10
+python main.py --embed --embed_dim 10000 --model SLDA --dataset cifar100
+python main.py --embed --embed_dim 10000 --model SLDA --dataset tinyimagenet_smol
+python main.py --embed --embed_dim 10000 --model SLDA --dataset miniimagenet_smol
+python main.py --embed --embed_dim 10000 --augment --model SLDA --dataset cifar10
+python main.py --embed --embed_dim 10000 --augment --model SLDA --dataset cifar100
+python main.py --embed --embed_dim 10000 --augment --model SLDA --dataset tinyimagenet_smol
+python main.py --embed --embed_dim 10000 --augment --model SLDA --dataset miniimagenet_smol
+python main.py --embed --embed_dim 10000 --model NCM --dataset mnist
+python main.py --embed --embed_dim 10000 --model NCM --dataset cifar10
+python main.py --embed --embed_dim 10000 --model NCM --dataset cifar100
+python main.py --embed --embed_dim 10000 --model NCM --dataset tinyimagenet_smol
+python main.py --embed --embed_dim 10000 --model NCM --dataset miniimagenet_smol
+python main.py --embed --embed_dim 10000 --augment --model NCM --dataset cifar10
+python main.py --embed --embed_dim 10000 --augment --model NCM --dataset cifar100
+python main.py --embed --embed_dim 10000 --augment --model NCM --dataset tinyimagenet_smol
+python main.py --embed --embed_dim 10000 --augment --model NCM --dataset miniimagenet_smol
+python main.py --embed --embed_dim 15000 --model SLDA --dataset mnist
+python main.py --embed --embed_dim 15000 --model SLDA --dataset cifar10
+python main.py --embed --embed_dim 15000 --model SLDA --dataset cifar100
+python main.py --embed --embed_dim 15000 --model SLDA --dataset tinyimagenet_smol
+python main.py --embed --embed_dim 15000 --model SLDA --dataset miniimagenet_smol
+python main.py --embed --embed_dim 15000 --augment --model SLDA --dataset cifar10
+python main.py --embed --embed_dim 15000 --augment --model SLDA --dataset cifar100
+python main.py --embed --embed_dim 15000 --augment --model SLDA --dataset tinyimagenet_smol
+python main.py --embed --embed_dim 15000 --augment --model SLDA --dataset miniimagenet_smol
+python main.py --embed --embed_dim 15000 --model NCM --dataset mnist
+python main.py --embed --embed_dim 15000 --model NCM --dataset cifar10
+python main.py --embed --embed_dim 15000 --model NCM --dataset cifar100
+python main.py --embed --embed_dim 15000 --model NCM --dataset tinyimagenet_smol
+python main.py --embed --embed_dim 15000 --model NCM --dataset miniimagenet_smol
+python main.py --embed --embed_dim 15000 --augment --model NCM --dataset cifar10
+python main.py --embed --embed_dim 15000 --augment --model NCM --dataset cifar100
+python main.py --embed --embed_dim 15000 --augment --model NCM --dataset tinyimagenet_smol
+python main.py --embed --embed_dim 15000 --augment --model NCM --dataset miniimagenet_smol
+python main.py --embed --embed_dim 20000 --model SLDA --dataset mnist
+python main.py --embed --embed_dim 20000 --model SLDA --dataset cifar10
+python main.py --embed --embed_dim 20000 --model SLDA --dataset cifar100
+python main.py --embed --embed_dim 20000 --model SLDA --dataset tinyimagenet_smol
+python main.py --embed --embed_dim 20000 --model SLDA --dataset miniimagenet_smol
+python main.py --embed --embed_dim 20000 --augment --model SLDA --dataset cifar10
+python main.py --embed --embed_dim 20000 --augment --model SLDA --dataset cifar100
+python main.py --embed --embed_dim 20000 --augment --model SLDA --dataset tinyimagenet_smol
+python main.py --embed --embed_dim 20000 --augment --model SLDA --dataset miniimagenet_smol
+python main.py --embed --embed_dim 20000 --model NCM --dataset mnist
+python main.py --embed --embed_dim 20000 --model NCM --dataset cifar10
+python main.py --embed --embed_dim 20000 --model NCM --dataset cifar100
+python main.py --embed --embed_dim 20000 --model NCM --dataset tinyimagenet_smol
+python main.py --embed --embed_dim 20000 --model NCM --dataset miniimagenet_smol
+python main.py --embed --embed_dim 20000 --augment --model NCM --dataset cifar10
+python main.py --embed --embed_dim 20000 --augment --model NCM --dataset cifar100
+python main.py --embed --embed_dim 20000 --augment --model NCM --dataset tinyimagenet_smol
+python main.py --embed --embed_dim 20000 --augment --model NCM --dataset miniimagenet_smol
+python main.py --embed --embed_dim 25000 --model SLDA --dataset mnist
+python main.py --embed --embed_dim 25000 --model SLDA --dataset cifar10
+python main.py --embed --embed_dim 25000 --model SLDA --dataset cifar100
+python main.py --embed --embed_dim 25000 --model SLDA --dataset tinyimagenet_smol
+python main.py --embed --embed_dim 25000 --model SLDA --dataset miniimagenet_smol
+python main.py --embed --embed_dim 25000 --augment --model SLDA --dataset cifar10
+python main.py --embed --embed_dim 25000 --augment --model SLDA --dataset cifar100
+python main.py --embed --embed_dim 25000 --augment --model SLDA --dataset tinyimagenet_smol
+python main.py --embed --embed_dim 25000 --augment --model SLDA --dataset miniimagenet_smol
+python main.py --embed --embed_dim 25000 --model NCM --dataset mnist
+python main.py --embed --embed_dim 25000 --model NCM --dataset cifar10
+python main.py --embed --embed_dim 25000 --model NCM --dataset cifar100
+python main.py --embed --embed_dim 25000 --model NCM --dataset tinyimagenet_smol
+python main.py --embed --embed_dim 25000 --model NCM --dataset miniimagenet_smol
+python main.py --embed --embed_dim 25000 --augment --model NCM --dataset cifar10
+python main.py --embed --embed_dim 25000 --augment --model NCM --dataset cifar100
+python main.py --embed --embed_dim 25000 --augment --model NCM --dataset tinyimagenet_smol
+python main.py --embed --embed_dim 25000 --augment --model NCM --dataset miniimagenet_smol
+python main.py --embed --embed_dim 30000 --model SLDA --dataset mnist
+python main.py --embed --embed_dim 30000 --model SLDA --dataset cifar10
+python main.py --embed --embed_dim 30000 --model SLDA --dataset cifar100
+python main.py --embed --embed_dim 30000 --model SLDA --dataset tinyimagenet_smol
+python main.py --embed --embed_dim 30000 --model SLDA --dataset miniimagenet_smol
+python main.py --embed --embed_dim 30000 --augment --model SLDA --dataset cifar10
+python main.py --embed --embed_dim 30000 --augment --model SLDA --dataset cifar100
+python main.py --embed --embed_dim 30000 --augment --model SLDA --dataset tinyimagenet_smol
+python main.py --embed --embed_dim 30000 --augment --model SLDA --dataset miniimagenet_smol
+python main.py --embed --embed_dim 30000 --model NCM --dataset mnist
+python main.py --embed --embed_dim 30000 --model NCM --dataset cifar10
+python main.py --embed --embed_dim 30000 --model NCM --dataset cifar100
+python main.py --embed --embed_dim 30000 --model NCM --dataset tinyimagenet_smol
+python main.py --embed --embed_dim 30000 --model NCM --dataset miniimagenet_smol
+python main.py --embed --embed_dim 30000 --augment --model NCM --dataset cifar10
+python main.py --embed --embed_dim 30000 --augment --model NCM --dataset cifar100
+python main.py --embed --embed_dim 30000 --augment --model NCM --dataset tinyimagenet_smol
+python main.py --embed --embed_dim 30000 --augment --model NCM --dataset miniimagenet_smol
+python main.py --embed --embed_dim 40000 --model SLDA --dataset mnist
+python main.py --embed --embed_dim 40000 --model SLDA --dataset cifar10
+python main.py --embed --embed_dim 40000 --model SLDA --dataset cifar100
+python main.py --embed --embed_dim 40000 --model SLDA --dataset tinyimagenet_smol
+python main.py --embed --embed_dim 40000 --model SLDA --dataset miniimagenet_smol
+python main.py --embed --embed_dim 40000 --augment --model SLDA --dataset cifar10
+python main.py --embed --embed_dim 40000 --augment --model SLDA --dataset cifar100
+python main.py --embed --embed_dim 40000 --augment --model SLDA --dataset tinyimagenet_smol
+python main.py --embed --embed_dim 40000 --augment --model SLDA --dataset miniimagenet_smol
+python main.py --embed --embed_dim 40000 --model NCM --dataset mnist
+python main.py --embed --embed_dim 40000 --model NCM --dataset cifar10
+python main.py --embed --embed_dim 40000 --model NCM --dataset cifar100
+python main.py --embed --embed_dim 40000 --model NCM --dataset tinyimagenet_smol
+python main.py --embed --embed_dim 40000 --model NCM --dataset miniimagenet_smol
+python main.py --embed --embed_dim 40000 --augment --model NCM --dataset cifar10
+python main.py --embed --embed_dim 40000 --augment --model NCM --dataset cifar100
+python main.py --embed --embed_dim 40000 --augment --model NCM --dataset tinyimagenet_smol
+python main.py --embed --embed_dim 40000 --augment --model NCM --dataset miniimagenet_smol
+python main.py --embed --embed_dim 50000 --model SLDA --dataset mnist
+python main.py --embed --embed_dim 50000 --model SLDA --dataset cifar10
+python main.py --embed --embed_dim 50000 --model SLDA --dataset cifar100
+python main.py --embed --embed_dim 50000 --model SLDA --dataset tinyimagenet_smol
+python main.py --embed --embed_dim 50000 --model SLDA --dataset miniimagenet_smol
+python main.py --embed --embed_dim 50000 --augment --model SLDA --dataset cifar10
+python main.py --embed --embed_dim 50000 --augment --model SLDA --dataset cifar100
+python main.py --embed --embed_dim 50000 --augment --model SLDA --dataset tinyimagenet_smol
+python main.py --embed --embed_dim 50000 --augment --model SLDA --dataset miniimagenet_smol
+python main.py --embed --embed_dim 50000 --model NCM --dataset mnist
+python main.py --embed --embed_dim 50000 --model NCM --dataset cifar10
+python main.py --embed --embed_dim 50000 --model NCM --dataset cifar100
+python main.py --embed --embed_dim 50000 --model NCM --dataset tinyimagenet_smol
+python main.py --embed --embed_dim 50000 --model NCM --dataset miniimagenet_smol
+python main.py --embed --embed_dim 50000 --augment --model NCM --dataset cifar10
+python main.py --embed --embed_dim 50000 --augment --model NCM --dataset cifar100
+python main.py --embed --embed_dim 50000 --augment --model NCM --dataset tinyimagenet_smol
+python main.py --embed --embed_dim 50000 --augment --model NCM --dataset miniimagenet_smol
 
+# No embedding for pretrained models
+python main.py --model SLDA --dataset vitbi1k_cifar100
+python main.py --model SLDA --dataset vitbi1k_imagenet-r
+python main.py --model SLDA --dataset vitbi1k_imagenet-a
+python main.py --model SLDA --dataset vitbi1k_cub
+python main.py --model SLDA --dataset vitbi1k_omnibenchmark
+python main.py --model SLDA --dataset vitbi1k_vtab
+python main.py --model SLDA --dataset vitbi21k_cifar100
+python main.py --model SLDA --dataset vitbi21k_imagenet-r
+python main.py --model SLDA --dataset vitbi21k_imagenet-a
+python main.py --model SLDA --dataset vitbi21k_cub
+python main.py --model SLDA --dataset vitbi21k_omnibenchmark
+python main.py --model SLDA --dataset vitbi21k_vtab
+python main.py --augment --model SLDA --dataset vitbi1k_cifar100
+python main.py --augment --model SLDA --dataset vitbi1k_imagenet-r
+python main.py --augment --model SLDA --dataset vitbi1k_imagenet-a
+python main.py --augment --model SLDA --dataset vitbi1k_cub
+python main.py --augment --model SLDA --dataset vitbi1k_omnibenchmark
+python main.py --augment --model SLDA --dataset vitbi1k_vtab
+python main.py --augment --model SLDA --dataset vitbi21k_cifar100
+python main.py --augment --model SLDA --dataset vitbi21k_imagenet-r
+python main.py --augment --model SLDA --dataset vitbi21k_imagenet-a
+python main.py --augment --model SLDA --dataset vitbi21k_cub
+python main.py --augment --model SLDA --dataset vitbi21k_omnibenchmark
+python main.py --augment --model SLDA --dataset vitbi21k_vtab
+python main.py --model NCM --dataset vitbi1k_cifar100
+python main.py --model NCM --dataset vitbi1k_imagenet-r
+python main.py --model NCM --dataset vitbi1k_imagenet-a
+python main.py --model NCM --dataset vitbi1k_cub
+python main.py --model NCM --dataset vitbi1k_omnibenchmark
+python main.py --model NCM --dataset vitbi1k_vtab
+python main.py --model NCM --dataset vitbi21k_cifar100
+python main.py --model NCM --dataset vitbi21k_imagenet-r
+python main.py --model NCM --dataset vitbi21k_imagenet-a
+python main.py --model NCM --dataset vitbi21k_cub
+python main.py --model NCM --dataset vitbi21k_omnibenchmark
+python main.py --model NCM --dataset vitbi21k_vtab
+python main.py --augment --model NCM --dataset vitbi1k_cifar100
+python main.py --augment --model NCM --dataset vitbi1k_imagenet-r
+python main.py --augment --model NCM --dataset vitbi1k_imagenet-a
+python main.py --augment --model NCM --dataset vitbi1k_cub
+python main.py --augment --model NCM --dataset vitbi1k_omnibenchmark
+python main.py --augment --model NCM --dataset vitbi1k_vtab
+python main.py --augment --model NCM --dataset vitbi21k_cifar100
+python main.py --augment --model NCM --dataset vitbi21k_imagenet-r
+python main.py --augment --model NCM --dataset vitbi21k_imagenet-a
+python main.py --augment --model NCM --dataset vitbi21k_cub
+python main.py --augment --model NCM --dataset vitbi21k_omnibenchmark
+python main.py --augment --model NCM --dataset vitbi21k_vtab
